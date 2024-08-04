721486b3e41bb602c78c3f77f9972626ba64db9a
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-08-04 15:20:30 +0530

    second navbar added

--- Insider_Trade/MainPage/script.js
+++ Insider_Trade/MainPage/script.js
@@ -5,17 +5,22 @@ document.getElementById("theme-toggle").addEventListener("click", function () {
   const logo = document.getElementById("logo");
   const heroImage = document.getElementById("heroImage");
   const footer = document.querySelector(".footer");
+  const secNav = document.querySelector(".second-nav");
 
   if (document.body.classList.contains("dark-mode")) {
     themeToggle.src = "../images/dark_button.svg";
     logo.src = "../images/logo_dark_theme.svg";
     footer.classList.remove("footer-light");
     footer.classList.add("footer-dark");
+    secNav.classList.remove("second-nav-light");
+    secNav.classList.add("second-nav-dark");
   } else {
     themeToggle.src = "../images/light_button.svg";
     logo.src = "../images/logo_light_theme.svg";
     footer.classList.remove("footer-dark");
     footer.classList.add("footer-light");
+    secNav.classList.remove("second-nav-dark");
+    secNav.classList.add("second-nav-light");
   }
 });
 
