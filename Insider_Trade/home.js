59625f55d0d2c5b9aa215deb720fd26b5a778dd5
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-08-07 13:17:42 +0530

    services dection added

--- Insider_Trade/home.js
+++ Insider_Trade/home.js
@@ -9,12 +9,12 @@ document.getElementById("theme-toggle").addEventListener("click", function () {
   const footer = document.querySelector(".footer");
 
   if (document.body.classList.contains("dark-mode")) {
-    themeToggle.src = "./images/dark_button.svg";
+    themeToggle.src = "./images/light_button.svg";
     // logo.src = "./images/logo_dark_theme.svg";
     footer.classList.remove("footer-light");
     footer.classList.add("footer-dark");
   } else {
-    themeToggle.src = "./images/light_button.svg";
+    themeToggle.src = "./images/dark_button.svg";
     // logo.src = "./images/logo_light_theme.svg";
     footer.classList.remove("footer-dark");
     footer.classList.add("footer-light");
@@ -28,4 +28,23 @@ window.addEventListener("scroll", function () {
   } else {
     navbar.classList.remove("scrolled");
   }
+});
+
+// contact ------------------------------------------------------
+document.getElementById("contactForm").addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  let name = document.getElementById("name").value;
+  let email = document.getElementById("email").value;
+  let phone = document.getElementById("phone").value;
+  let findUs = document.getElementById("findUs").value;
+
+  if (!name || !phone) {
+    alert("Please fill in the required fields.");
+    return;
+  }
+
+  // Additional validation logic can go here
+
+  alert("Form submitted successfully!");
 });
