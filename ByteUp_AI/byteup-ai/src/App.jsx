648758dc9825fa0ae0bdee222f50cdf182a4799e
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-08-14 22:15:14 +0530

    fonts added

--- ByteUp_AI/byteup-ai/src/App.jsx
+++ ByteUp_AI/byteup-ai/src/App.jsx
@@ -1,10 +1,12 @@
 const App = () => {
   return (
-    <div className="bg-black text-white">
-      App
-      <h1>Tailwind Setup</h1>
+    <div className="bg-black text-white font-Syne text-5xl p-20">
+      Network Structure Design
+      <h1 className="text-purple-500 font-Heebo uppercase text-sm mt-10">
+        Home
+      </h1>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
--- ByteUp_AI/byteup-ai/tailwind.config.js
+++ ByteUp_AI/byteup-ai/tailwind.config.js
@@ -1,12 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Archivo: ["Archivo", "sans-serif"],
+        Syne: ["Syne", "sans-serif"],
+        Heebo: ["Heebo", "sans-serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
