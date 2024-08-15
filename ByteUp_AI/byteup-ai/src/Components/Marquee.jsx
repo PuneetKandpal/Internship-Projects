ba5a071d122dc86c688df9b1ba75bb77eceb05cb
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-08-15 22:47:55 +0530

    marquee component created

---
+++ ByteUp_AI/byteup-ai/src/Components/Marquee.jsx
@@ -0,0 +1,34 @@
+// import { useContext } from "react";
+// import { ThemeContext } from "../Context/ThemeContext";
+
+const Marquee = () => {
+  //   const { theme } = useContext(ThemeContext);
+
+  return (
+    <div className="py-16">
+      <div className="moving-text">
+        <div className="container font-Syne">
+          <h1>Generative AI</h1>
+          <h1>Generative AI</h1>
+          <h1>Generative AI</h1>
+          <h1>Generative AI</h1>
+          <h1>Generative AI</h1>
+          <h1>Generative AI</h1>
+          {/* <h1>Web scrapping</h1> */}
+          {/* <h1>Automation</h1> */}
+          {/* <h1>Python API Development</h1> */}
+          {/* <h1>SaaS Development</h1> */}
+
+          {/* <h1>Generative AI</h1> */}
+          {/* <h1>Web scrapping</h1> */}
+          {/* <h1>Automation</h1> */}
+          {/* <h1>Python API Development</h1> */}
+          {/* <h1>SaaS Development</h1> */}
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Marquee;
--- ByteUp_AI/byteup-ai/src/Pages/Home.jsx
+++ ByteUp_AI/byteup-ai/src/Pages/Home.jsx
@@ -1,11 +1,13 @@
 import Hero from "../Components/Hero";
+// import Marquee from "../Components/Marquee";
 import Navbar from "../Components/Navbar";
 
 const Home = () => {
   return (
     <div>
       <Navbar />
-      <Hero/>
+      <Hero />
+      {/* <Marquee /> */}
     </div>
   );
 };
