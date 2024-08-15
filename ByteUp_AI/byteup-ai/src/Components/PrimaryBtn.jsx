6ce5294aeb5f022ebdf2c3e204aec6278a726287
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-08-15 19:45:16 +0530

    Primary button created

---
+++ ByteUp_AI/byteup-ai/src/Components/PrimaryBtn.jsx
@@ -0,0 +1,11 @@
+const PrimaryBtn = () => {
+  return (
+    <div>
+      <button className="border border-white text-white text-sm uppercase  font-Archivo tracking-[1.5px] font-light px-6 py-3 hover:text-black hover:bg-white transition ease-in-out duration-200">
+        Get Started
+      </button>
+    </div>
+  );
+};
+
+export default PrimaryBtn;
