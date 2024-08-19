942142efdea8b431a0302c94608406505b3647bc
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-08-19 21:16:40 +0530

    footer added to contact page

--- ByteUp_AI/byteup-ai/src/Pages/Contact.jsx
+++ ByteUp_AI/byteup-ai/src/Pages/Contact.jsx
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "../Components/Navbar";
 import ContactUs from "../Components/ContactUs";
+import Footer from "../Components/Footer";
 
 const Contact = () => {
   return (
     <div>
       <Navbar withHeroSection={false} />
       <ContactUs />
+
+      <Footer />
     </div>
   );
 };
