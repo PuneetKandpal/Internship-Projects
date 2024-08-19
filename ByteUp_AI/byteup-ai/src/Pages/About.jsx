2f87a99755b0de5aa6822216613a9034803ed0e1
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-08-19 15:08:41 +0530

    Team and Footer component added to about

--- ByteUp_AI/byteup-ai/src/Pages/About.jsx
+++ ByteUp_AI/byteup-ai/src/Pages/About.jsx
@@ -4,6 +4,8 @@ import { ThemeContext } from "../Context/ThemeContext";
 import AboutHero from "../Components/AboutHero";
 import About3Points from "../Components/About3Points";
 import AboutClients from "../Components/AboutClients";
+import Team from "../Components/Team";
+import Footer from "../Components/Footer";
 
 const About = () => {
   const { theme } = useContext(ThemeContext);
@@ -15,8 +17,10 @@ const About = () => {
     >
       <Navbar withHeroSection={false} />
       <AboutHero />
-      <About3Points/>
-      <AboutClients/>
+      <About3Points />
+      <AboutClients />
+      <Team />
+      <Footer />
     </div>
   );
 };
