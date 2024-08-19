9a52c4c90c673a0484043f50652fd20f171ee636
Vaibhav <vaibhavjam86@gmail.com>
2024-08-20 00:23:40 +0530

    Add ReachOut component

--- ByteUp_AI/byteup-ai/src/Pages/Contact.jsx
+++ ByteUp_AI/byteup-ai/src/Pages/Contact.jsx
@@ -1,14 +1,14 @@
-
 import Navbar from "../Components/Navbar";
 import ContactUs from "../Components/ContactUs";
 import Footer from "../Components/Footer";
+import ReachOut from "../Components/ReachOut";
 
 const Contact = () => {
   return (
     <div>
       <Navbar withHeroSection={false} />
       <ContactUs />
-
+      <ReachOut />
       <Footer />
     </div>
   );
