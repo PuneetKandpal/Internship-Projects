a478bea13b4be85703463d6d8a59a84ba1e147bd
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-08-23 20:40:08 +0530

    navbar js added in stocks page

--- Uranium_Tracker/stocks.js
+++ Uranium_Tracker/stocks.js
@@ -1,3 +1,31 @@
+// Navbar ====================================
+
+const navToggle = document.getElementById("nav-toggle");
+const mobileMenu = document.getElementById("mobile-menu");
+
+navToggle.addEventListener("click", () => {
+  if (mobileMenu.classList.contains("hidden")) {
+    // Open menu animation
+    mobileMenu.classList.remove("hidden");
+    gsap.fromTo(
+      mobileMenu,
+      { scaleY: 0, opacity: 0 },
+      { scaleY: 1, opacity: 1, duration: 0.5, ease: "power2.out" }
+    );
+  } else {
+    // Close menu animation
+    gsap.to(mobileMenu, {
+      scaleY: 0,
+      opacity: 0,
+      duration: 0.5,
+      ease: "power2.in",
+      onComplete: () => {
+        mobileMenu.classList.add("hidden");
+      },
+    });
+  }
+});
+
 /**table */
 document.addEventListener("DOMContentLoaded", () => {
   // Select all percentage cells for Change (1M) and Change (1Y)
