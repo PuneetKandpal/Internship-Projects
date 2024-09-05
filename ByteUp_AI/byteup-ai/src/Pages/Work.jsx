c7a035dfec5832a3e0533bb6f16e9193456fee75
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-09-05 19:49:53 +0530

    Work.jsx : marquee added

--- ByteUp_AI/byteup-ai/src/Pages/Work.jsx
+++ ByteUp_AI/byteup-ai/src/Pages/Work.jsx
@@ -1,14 +1,17 @@
-import Navbar from "../Components/Navbar"
-import WorkHero from "../Components/workHero"
-
+import Navbar from "../Components/Navbar";
+import WorkHero from "../Components/workHero";
+import Marquee from "../Components/Marquee";
 
 const Work = () => {
   return (
     <div>
-        <Navbar/>
-        <WorkHero/>
+      <Navbar />
+      <WorkHero />
+      <div className="mt-[-40px]">
+        <Marquee />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Work
+export default Work;
