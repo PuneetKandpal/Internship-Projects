2a3a6c495f15ffc85a451e0da10ed62efe679755
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-09-09 12:43:29 +0530

    responsive : WorkHero.jsx

--- ByteUp_AI/byteup-ai/src/Components/WorkHero.jsx
+++ ByteUp_AI/byteup-ai/src/Components/WorkHero.jsx
@@ -35,7 +35,6 @@ const WorkHero = () => {
       }
     );
 
-    // Count-up animation
     let ctx = gsap.context(() => {
       let countAnimation = { val: 0 };
       gsap.to(countAnimation, {
@@ -57,17 +56,18 @@ const WorkHero = () => {
 
   return (
     <div
-      className={`relative min-h-[72vh] w-full py-20 px-[5.5rem] flex flex-col justify-center text-left bg-cover bg-center ${
+      className={`relative min-h-[72vh] w-full py-10 sm:py-16 md:py-20 px-6 sm:px-12 md:px-[5.5rem] flex flex-col justify-center text-left bg-cover bg-center ${
         theme === "light" ? "bg-white text-black" : "bg-black text-white"
       }`}
       style={{ backgroundImage: `url(${backgroundUrl})` }}
     >
       <AnimatedCursor isHovered={isHovered} />
+
       {/* Gradient Overlay */}
       <div className="absolute inset-0 bg-gradient-to-r from-black/75 via-black/60 to-transparent z-10"></div>
 
       {/* Content */}
-      <div className="relative z-20 w-[60%]" ref={contentRef}>
+      <div className="relative z-20 w-full md:w-[60%]" ref={contentRef}>
         <h1
           onMouseEnter={() => {
             setIsHovered(true);
@@ -75,20 +75,18 @@ const WorkHero = () => {
           onMouseLeave={() => {
             setIsHovered(false);
           }}
-          className="text-5xl font-bold leading-10 mb-3"
+          className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight mb-5 md:mb-8"
         >
-          <span className="text-white font-Syne text-5xl">
-          We build top-notch solutions for brand , web and mobile apps.
+          <span className="text-white font-Syne">
+            We build top-notch solutions for brand, web, and mobile apps.
           </span>
         </h1>
 
-        <div className={`text-white mb-8`}>
-          <h1 ref={countRef} className="text-[2.9rem] font-bold">
+        <div className="text-white mb-9 md:mb-8">
+          <h1 ref={countRef} className="text-4xl sm:text-5xl font-bold">
             {clientCount}+
           </h1>
-          <h2
-            className={`text-[13px] mt-[-0.4rem] uppercase font-Syne leading-4 font-normal ml-1 tracking-[.20em] text-white/70`}
-          >
+          <h2 className="text-sm  mt-2 md:mt-1 uppercase font-Syne leading-4 font-normal tracking-[.1em] ml-1 text-white/70">
             Happy Clients and Counting
           </h2>
         </div>
