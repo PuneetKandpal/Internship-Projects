39cb0726dcab058d7fb4456ec3825fe9620b687e
Vaibhav <vaibhavjam86@gmail.com>
2024-09-13 16:31:57 +0530

    Table added

--- Uranium_Tracker_React/uranium-tracker/src/pages/Investments.jsx
+++ Uranium_Tracker_React/uranium-tracker/src/pages/Investments.jsx
@@ -1,11 +1,25 @@
-
+import Navbar from "../components/Navbar";
+import InvestmentsSidenav from "../components/InvestmentsSidenav";
+import InvestmentsTopPerformers from "../components/InvestmentsTopPerformers";
+import InvestmentsAtomicPortfolio from "../components/InvestmentsAtomicPortfolio";
+import InvestmentUraniumTable from "../components/InvestmentsUraniumTable";
+import Footer from "../components/Footer";
 
 const Investments = () => {
   return (
     <div>
-      Investments
+      <Navbar />
+      <div class="px-6 md:px-20 mx-auto w-full flex justify-between relative">
+        <InvestmentsSidenav />
+        <div class="sidenav-animation w-full md:w-[78%] mt-[6.9rem] md:ml-[24%] overflow-y-auto">
+          <InvestmentsTopPerformers />
+          <InvestmentsAtomicPortfolio />
+          <InvestmentUraniumTable />
+        </div>
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default Investments
+export default Investments;
