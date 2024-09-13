2d0774a641b045f05003048a6bd41eef96e58c21
Vaibhav <vaibhavjam86@gmail.com>
2024-09-13 20:33:37 +0530

    Modified e7 page

--- Uranium_Tracker_React/uranium-tracker/src/pages/essential_subpages/Eseven.jsx
+++ Uranium_Tracker_React/uranium-tracker/src/pages/essential_subpages/Eseven.jsx
@@ -1,14 +1,18 @@
-import Breadcrumb from "../../components/essential_components/breadcrumb"
-import EssentialSubHero from "../../components/essential_components/EssentialSubHero"
-
+import Breadcrumb from "../../components/essential_components/breadcrumb";
+import EssentialSubHero from "../../components/essential_components/EssentialSubHero";
 
 const Eseven = () => {
   return (
     <div>
-      <Breadcrumb link={"/essentials/e7"} />
-      <EssentialSubHero heading={"What"} subheading={"Uranium"} />
+      <Breadcrumb link={"/essentials/e7"} title={"Uranium Processing"} />
+      <EssentialSubHero
+        heading={"Uranium Processing"}
+        subheading={
+          "Uranium processing is the critical step that transforms raw uranium ore into a refined form capable of fueling nuclear reactors.This intricate process ensures that uranium's immense energy potential is harnessed efficiently, powering the world with clean and reliable energy"
+        }
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Eseven
+export default Eseven;
