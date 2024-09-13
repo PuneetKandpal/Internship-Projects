9b6a12c94cb0471c7dab87a680e323e4dc81d263
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-09-13 09:03:11 +0530

    project setup

--- Uranium_Tracker_React/uranium-tracker/tailwind.config.js
+++ Uranium_Tracker_React/uranium-tracker/tailwind.config.js
@@ -1,9 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        white: "#FFFFFF",
+        lime1: "#57fec7",
+        lime2: "#27ecab",
+        black: "#19310B",
+        black1: "#161616",
+      },
+    },
   },
   plugins: [],
-}
-
+};
