d7737c06ad3b6de53a60bfc0b2fac0c70058119a
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-09-14 16:30:08 +0530

    NewsHero section fixed (API fetching corrected)

--- Uranium_Tracker_React/uranium-tracker/src/components/NewsHero.jsx
+++ Uranium_Tracker_React/uranium-tracker/src/components/NewsHero.jsx
@@ -8,7 +8,7 @@ import Loader from "./Loader"; // Import your loader component
 
 const NewsHero = () => {
   const dispatch = useDispatch();
-  const newsData = useSelector((state) => state.api.home);
+  const newsData = useSelector((state) => state.api.news);
   const status = useSelector((state) => state.api.status);
   const [isLoading, setIsLoading] = useState(true); // Initialize isLoading state
 
@@ -62,97 +62,93 @@ const NewsHero = () => {
         <div className="grid grid-cols-1 lg:grid-cols-3 gap-[2px] h-full">
           {/* Left Section - Large Image */}
           <div className="lg:col-span-2 h-full">
-            {newsData.featured_news && newsData.featured_news.length > 0 && (
-              <Link
-                to={`/news/${newsData.featured_news[0].id}`}
-                className="block h-full"
-              >
-                <div className="relative h-[559px] group overflow-hidden animate-large-img">
-                  <img
-                    src={newsData.featured_news[0].image_url}
-                    alt={newsData.featured_news[0].title}
-                    className="w-full h-full object-cover group-hover:scale-105 transition-all duration-500 object-bottom"
-                  />
-                  <div className="absolute inset-0 bg-gradient-to-t from-[#111111] via-[#111111]/40 to-transparent"></div>
+            <Link to="/news/1" className="block h-full">
+              {" "}
+              {/* Link to individual news page */}
+              <div className="relative h-[559px] group overflow-hidden animate-large-img">
+                <img
+                  src={newsData.featured_news.image_url}
+                  alt="Oculus Quest X Headset"
+                  className="w-full h-full object-cover group-hover:scale-105 transition-all duration-500 object-bottom"
+                />
+                <div className="absolute inset-0 bg-gradient-to-t from-[#111111] via-[#111111]/40 to-transparent"></div>
+                <div className="absolute bottom-6 left-5 text-white z-10">
+                  <span className="bg-lime1 text-black font-semibold text-xs px-2 py-1 uppercase">
+                    {newsData.featured_news.publisher}
+                  </span>
+                  <h2 className="text-2xl font-semibold pr-6 mt-4 leading-[35px]">
+                    {newsData.featured_news.title}
+                  </h2>
+                  <div className="flex gap-x-3 items-center">
+                    <p className="text-[13.5px] lato text-white/80 mt-5">
+                      {newsData.featured_news.author} –{" "}
+                    </p>
 
-                  <div className="absolute bottom-6 left-5 text-white z-10">
-                    <span className="bg-lime1 text-black font-semibold text-xs px-2 py-1 uppercase">
-                      {newsData.featured_news[0].publisher}
-                    </span>
-                    <h2 className="text-2xl font-semibold pr-6 mt-4 leading-[35px]">
-                      {newsData.featured_news[0].title}
-                    </h2>
-                    <div className="flex gap-x-3 mt-5">
-                      <p className="text-[13.5px] lato text-white/80">
-                        {newsData.featured_news[0].author === "N/A"
-                          ? "No Author"
-                          : newsData.featured_news[0].author}{" "}
-                        -{" "}
-                      </p>
-
-                      <p className="text-[13.5px] lato text-white/80">
-                        {new Date(
-                          newsData.featured_news[0].published_date
-                            .split("-")
-                            .reverse()
-                            .join("-")
-                        ).toLocaleDateString("en-US", {
-                          month: "short",
-                          day: "numeric",
-                          year: "numeric",
-                        })}
-                      </p>
-                    </div>
+                    <p className="text-[13.5px] lato text-white/80 mt-5">
+                      {new Date(
+                        newsData.featured_news.published_date
+                          .split("-")
+                          .reverse()
+                          .join("-")
+                      ).toLocaleDateString("en-US", {
+                        month: "short",
+                        day: "numeric",
+                        year: "numeric",
+                      })}
+                    </p>
                   </div>
                 </div>
-              </Link>
-            )}
+              </div>
+            </Link>
           </div>
 
           {/* Right Section - Stacked Smaller Images */}
           <div className="flex flex-col space-y-1 animate-small-imgs">
-            {newsData.featured_news &&
-              newsData.featured_news.slice(1, 3).map((newsItem) => (
-                <Link to={`/news/${newsItem.id}`} key={newsItem.id}>
-                  <div className="relative w-full h-[277px] group overflow-hidden">
-                    <img
-                      src={newsItem.image_url}
-                      alt={newsItem.title}
-                      className="w-full h-full object-cover group-hover:scale-105 transition-all duration-500"
-                    />
-                    <div className="absolute inset-0 bg-gradient-to-t from-[#111111] via-[#111111]/40 to-transparent"></div>
-                    <div className="absolute bottom-6 left-5 text-white z-10">
-                      <span className="bg-lime1 text-black font-semibold text-[11px] px-2 py-1 uppercase">
-                        {newsItem.publisher}
-                      </span>
-                      <h3 className="text-[20px] leading-[25px] font-semibold mt-2 pr-4">
-                        {newsItem.title}
-                      </h3>
-                      <div className="flex gap-x-3 mt-5">
-                        <p className="text-[13.5px] lato text-white/80">
-                          {newsItem.author === "N/A"
-                            ? "No Author"
-                            : newsData.featured_news[0].author}{" "}
-                          -{" "}
-                        </p>
+            {/* Top Small Image */}
+            <Link to="/news/2">
+              <div className="relative w-full h-[277px] group overflow-hidden">
+                <img
+                  src="https://www.mining.com/wp-content/uploads/2024/01/Uranium_Energy_Corp_Uranium_Energy_Corp_Completes_Restart_Progra-300x207.jpg"
+                  alt="Uranium Energy"
+                  className="w-full h-full object-cover group-hover:scale-105 transition-all duration-500"
+                />
+                <div className="absolute inset-0 bg-gradient-to-t from-[#111111] via-[#111111]/40 to-transparent"></div>
+                <div className="absolute bottom-6 left-5 text-white z-10">
+                  <span className="bg-lime1 text-black font-semibold text-[11px] px-2 py-1 uppercase">
+                    Technology
+                  </span>
+                  <h3 className="text-[20px] leading-[25px] font-semibold mt-2 pr-4">
+                    Uranium Energy restarts uranium production in Wyoming
+                  </h3>
+                  <p className="text-xs mt-3 text-white/80 lato">
+                    Mining.com Doe – Aug 21, 2024
+                  </p>
+                </div>
+              </div>
+            </Link>
 
-                        <p className="text-[13.5px] lato text-white/80">
-                          {new Date(
-                            newsItem.published_date
-                              .split("-")
-                              .reverse()
-                              .join("-")
-                          ).toLocaleDateString("en-US", {
-                            month: "short",
-                            day: "numeric",
-                            year: "numeric",
-                          })}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                </Link>
-              ))}
+            {/* Bottom Small Image */}
+            <Link to="/news/3">
+              <div className="relative w-full h-[277px] group overflow-hidden">
+                <img
+                  src="https://images.unsplash.com/photo-1567665202038-6c5e97837696?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8dXJhbml1bXxlbnwwfHwwfHx8MA%3D%3D"
+                  alt="Fujifilm’s 102-Megapixel Camera"
+                  className="w-full h-full object-cover group-hover:scale-105 transition-all duration-500"
+                />
+                <div className="absolute inset-0 bg-gradient-to-t from-[#111111] via-[#111111]/40 to-transparent"></div>
+                <div className="absolute bottom-6 left-5 text-white z-10">
+                  <span className="bg-lime1 text-black font-semibold text-[11px] px-2 py-1 uppercase">
+                    Technology
+                  </span>
+                  <h3 className="text-[20px] leading-[25px] font-semibold mt-2 pr-4">
+                    ERA scores win in battle to keep Jabikula uranium lease
+                  </h3>
+                  <p className="text-xs mt-3 text-white/80 lato">
+                    Mining.com Doe – Aug 21, 2024
+                  </p>
+                </div>
+              </div>
+            </Link>
           </div>
         </div>
       </div>
