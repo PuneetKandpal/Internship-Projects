a242c97918e39b4317df01f35e5aee7a128eb7c4
Vaibhav <vaibhavjam86@gmail.com>
2024-09-14 14:23:13 +0530

    Fetched marquee data from api

--- Uranium_Tracker_React/uranium-tracker/src/components/Marquee.jsx
+++ Uranium_Tracker_React/uranium-tracker/src/components/Marquee.jsx
@@ -1,32 +1,55 @@
+import { useEffect, useState } from "react";
 import Marquee from "react-fast-marquee";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchHomeData } from "../store/slices/apiSlice";
+import Loader from "./Loader"; // Import your loader component
 
 const StockMarquee = () => {
+  const dispatch = useDispatch();
+  const homeData = useSelector((state) => state.api.home);
+  const status = useSelector((state) => state.api.status);
+  const [isLoading, setIsLoading] = useState(true); // Initialize isLoading state
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchHomeData());
+    }
+
+    if (status === "succeeded") {
+      setIsLoading(false); // Set loading to false when data is fetched
+    }
+  }, [status, dispatch]);
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
   return (
     <div className="my-0 py-14 sm:py-20 pb-20 sm:pb-28 lato w-full h-[35px] overflow-hidden">
       <Marquee
         pauseOnHover={true} // This will stop the marquee when hovered
-        speed={80} // Adjust the speed as needed
-        gradient={true} // Disable gradient to avoid white space on the edges
+        speed={90} // Adjust the speed as needed
+        gradient={true} // Enable gradient
         gradientColor={"#161616"}
         gradientWidth={300}
       >
-        <span className="font-semibold">URA </span>$25.73
-        <span className="text-green-600 font-semibold"> ▲0.35%</span>
-        <span className="ml-12 font-semibold">CCJ </span>$41.41
-        <span className="text-green-600 font-semibold"> ▲0.07%</span>
-        <span className="ml-12 font-semibold">DNN </span>$1.57
-        <span className="text-green-600 font-semibold"> ▲0.64%</span>
-        <span className="ml-12 font-semibold">UUUU </span>$4.86
-        <span className="text-green-600 font-semibold"> ▲2.97%</span>
-        <span className="ml-12 font-semibold">UEC </span>$5.04
-        <span className="text-green-600 font-semibold"> ▲0.40%</span>
-        <span className="ml-12 font-semibold">NXE </span>$5.99
-        <span className="text-red-600 font-semibold"> ▼0.17%</span>
-        <span className="ml-12 font-semibold">UROY </span>$2.10
-        <span className="text-red-600 font-semibold"> ▼1.87%</span>
-        <span className="ml-12 font-semibold">U-UN.TO </span>$23.57
-        <span className="text-green-600 font-semibold"> ▲1.20%</span>
-        {/* Add more stock items as needed */}
+        {homeData.stocks.map((stock, index) => {
+          const { ticker, current_price, change_1m } = stock;
+          const changeColor =
+            change_1m >= 0 ? "text-green-600" : "text-red-600"; // Determine color based on change
+
+          return (
+            <span key={index} className="ml-12 font-semibold">
+              {ticker} ${current_price.toFixed(2)}
+              <span className={`${changeColor} font-semibold`}>
+                {" "}
+                {change_1m >= 0
+                  ? `▲${(change_1m * 100).toFixed(2)}%`
+                  : `▼${Math.abs(change_1m * 100).toFixed(2)}%`}
+              </span>
+            </span>
+          );
+        })}
       </Marquee>
     </div>
   );
