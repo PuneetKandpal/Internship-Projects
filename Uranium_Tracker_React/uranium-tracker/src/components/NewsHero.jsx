02bc248f50d867735872cd36c306c8cd2c6d6b18
Vaibhav <vaibhavjam86@gmail.com>
2024-09-15 10:20:59 +0530

    updated news-hero componenet

--- Uranium_Tracker_React/uranium-tracker/src/components/NewsHero.jsx
+++ Uranium_Tracker_React/uranium-tracker/src/components/NewsHero.jsx
@@ -21,34 +21,34 @@ const NewsHero = () => {
       setIsLoading(false); // Set loading to false when data is fetched
     }
 
-    gsap.registerPlugin(ScrollTrigger);
+    // gsap.registerPlugin(ScrollTrigger);
 
-    // Animate the large image section
-    gsap.from(".animate-large-img", {
-      opacity: 0,
-      y: 50,
-      duration: 1.5,
-      ease: "power4.out",
-      scrollTrigger: {
-        trigger: ".animate-large-img",
-        start: "top 80%",
-        toggleActions: "play none none none",
-      },
-    });
+    // // Animate the large image section
+    // gsap.from(".animate-large-img", {
+    //   opacity: 0,
+    //   y: 50,
+    //   duration: 1.5,
+    //   ease: "power4.out",
+    //   scrollTrigger: {
+    //     trigger: ".animate-large-img",
+    //     start: "top 80%",
+    //     toggleActions: "play none none none",
+    //   },
+    // });
 
-    // Animate the smaller image sections
-    gsap.from(".animate-small-imgs > *", {
-      opacity: 0,
-      y: 30,
-      duration: 1,
-      ease: "power4.out",
-      stagger: 0.3,
-      scrollTrigger: {
-        trigger: ".animate-small-imgs",
-        start: "top 80%",
-        toggleActions: "play none none none",
-      },
-    });
+    // // Animate the smaller image sections
+    // gsap.from(".animate-small-imgs > *", {
+    //   opacity: 0,
+    //   y: 30,
+    //   duration: 1,
+    //   ease: "power4.out",
+    //   stagger: 0.3,
+    //   scrollTrigger: {
+    //     trigger: ".animate-small-imgs",
+    //     start: "top 80%",
+    //     toggleActions: "play none none none",
+    //   },
+    // });
   }, [status, dispatch, isLoading]);
 
   // Show the loader while loading is true
@@ -64,44 +64,44 @@ const NewsHero = () => {
           <div className="lg:col-span-2 h-full">
             <Link
               to={`${
-                newsData.featured_news.link
+                newsData?.featured_news?.link
                   ? newsData.featured_news.link
                   : "https://www.mining-technology.com/news/putin-calls-for-export-limits-on-russian-nickel-uranium-and-titanium/"
-              } `}
+              }`}
               className="block h-full"
             >
-              {" "}
               {/* Link to individual news page */}
               <div className="relative h-[559px] group overflow-hidden animate-large-img">
                 <img
-                  src={newsData.featured_news.image_url}
+                  src={newsData?.featured_news?.image_url || ""}
                   alt="Oculus Quest X Headset"
                   className="w-full h-full object-cover group-hover:scale-105 transition-all duration-500 object-bottom"
                 />
                 <div className="absolute inset-0 bg-gradient-to-t from-[#111111] via-[#111111]/40 to-transparent"></div>
                 <div className="absolute bottom-6 left-5 text-white z-10">
                   <span className="bg-lime1 text-black font-semibold text-xs px-2 py-1 uppercase">
-                    {newsData.featured_news.publisher}
+                    {newsData?.featured_news?.publisher || "Unknown"}
                   </span>
                   <h2 className="text-2xl font-semibold pr-6 mt-4 leading-[35px]">
-                    {newsData.featured_news.title}
+                    {newsData?.featured_news?.title || "No Title Available"}
                   </h2>
                   <div className="flex gap-x-3 items-center">
                     <p className="text-[13.5px] lato text-white/80 mt-5">
-                      {newsData.featured_news.author} –{" "}
+                      {newsData?.featured_news?.author || "Unknown"} –{" "}
                     </p>
-
                     <p className="text-[13.5px] lato text-white/80 mt-5">
-                      {new Date(
-                        newsData.featured_news.published_date
-                          .split("-")
-                          .reverse()
-                          .join("-")
-                      ).toLocaleDateString("en-US", {
-                        month: "short",
-                        day: "numeric",
-                        year: "numeric",
-                      })}
+                      {newsData?.featured_news?.published_date
+                        ? new Date(
+                            newsData.featured_news.published_date
+                              .split("-")
+                              .reverse()
+                              .join("-")
+                          ).toLocaleDateString("en-US", {
+                            month: "short",
+                            day: "numeric",
+                            year: "numeric",
+                          })
+                        : "Unknown Date"}
                     </p>
                   </div>
                 </div>
