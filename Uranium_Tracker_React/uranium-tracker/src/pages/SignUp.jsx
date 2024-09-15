a1f0867bc693518a93786180481388a9ed2a0677
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-09-15 17:48:14 +0530

    responsive: login, signup, forgot password

--- Uranium_Tracker_React/uranium-tracker/src/pages/SignUp.jsx
+++ Uranium_Tracker_React/uranium-tracker/src/pages/SignUp.jsx
@@ -63,16 +63,15 @@ const SignUp = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     if (validate()) {
-      // Handle successful signup logic here
       console.log("Sign Up successful");
     }
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-100">
-      <div className="bg-white w-[60%] rounded-lg shadow-lg p-8 flex">
+    <div className="min-h-screen flex items-center justify-center  p-4 md:p-0">
+      <div className="bg-zinc-800/20 w-full md:w-[60%] rounded-lg shadow-lg p-8 flex flex-col md:flex-row">
         {/* Left Side */}
-        <div className="hidden md:flex flex-col justify-center w-1/2 bg-green-400 rounded-l-lg p-8 text-white">
+        <div className="hidden md:flex flex-col justify-center w-full md:w-1/2 bg-green-400 rounded-l-lg p-8 text-white">
           <h2 className="text-[29px] font-bold">Join Us!</h2>
           <p className="mt-2">
             Create an account to enjoy all the features of our service.
@@ -82,25 +81,25 @@ const SignUp = () => {
             to="/login"
             className="mt-2 w-fit bg-white text-green-500 py-2 px-6 rounded font-semibold hover:bg-gray-200"
           >
-            LogIn
+            Log In
           </NavLink>
         </div>
 
         {/* Right Side (SignUp Form) */}
-        <div className="w-full md:w-1/2 p-8">
+        <div className="w-full md:w-1/2 p-4 md:p-8">
           <h2 className="text-2xl font-semibold text-center mb-6">Sign Up</h2>
 
           <form onSubmit={handleSubmit}>
             {/* Name Field */}
-            <div className="mb-5">
-              <label className="text-gray-600 flex items-center mb-1">
+            <div className="mb-6">
+              <label className="text-zinc-300 flex items-center mb-1 text-[15px]">
                 <FaUser className="mr-2" /> Name
               </label>
               <input
                 type="text"
-                className={`w-full px-4 py-2 border text-black ${
+                className={`w-full px-4 py-[7px] bg-zinc-800/50 text-white placeholder ${
                   errors.name ? "border-red-500" : "border-gray-300"
-                } rounded focus:outline-none focus:ring-2 focus:ring-green-500`}
+                } rounded focus:outline-none focus:ring-1 focus:ring-green-500`}
                 value={name}
                 onChange={(e) => setName(e.target.value)}
                 placeholder="Enter your name"
@@ -111,15 +110,15 @@ const SignUp = () => {
             </div>
 
             {/* Email Field */}
-            <div className="mb-5">
-              <label className="text-gray-600 flex items-center mb-1">
+            <div className="mb-6">
+              <label className="text-zinc-300 flex items-center mb-1 text-[15px]">
                 <FaEnvelope className="mr-2 mt-[2px]" /> Email
               </label>
               <input
                 type="text"
-                className={`w-full px-4 py-2 border text-black ${
+                className={`w-full px-4 py-[7px] bg-zinc-800/50 text-white placeholder ${
                   errors.email ? "border-red-500" : "border-gray-300"
-                } rounded focus:outline-none focus:ring-2 focus:ring-green-500`}
+                } rounded focus:outline-none focus:ring-1 focus:ring-green-500`}
                 value={email}
                 onChange={(e) => setEmail(e.target.value)}
                 placeholder="Enter your email"
@@ -130,15 +129,15 @@ const SignUp = () => {
             </div>
 
             {/* Password Field */}
-            <div className="mb-5">
-              <label className="text-gray-600 flex items-center mb-1">
+            <div className="mb-6">
+              <label className="text-zinc-300 flex items-center mb-1 text-[15px]">
                 <FaLock className="mr-2" /> Password
               </label>
               <input
                 type="password"
-                className={`w-full px-4 py-2 border text-black ${
+                className={`w-full px-4 py-[7px] bg-zinc-800/50 text-white placeholder ${
                   errors.password ? "border-red-500" : "border-gray-300"
-                } rounded focus:outline-none focus:ring-2 focus:ring-green-500`}
+                } rounded focus:outline-none focus:ring-1 focus:ring-green-500`}
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 placeholder="Enter your password"
@@ -149,15 +148,15 @@ const SignUp = () => {
             </div>
 
             {/* Confirm Password Field */}
-            <div className="mb-5">
-              <label className="text-gray-600 flex items-center mb-1">
+            <div className="mb-6">
+              <label className="text-zinc-300 flex items-center mb-1 text-[15px]">
                 <FaLock className="mr-2" /> Confirm Password
               </label>
               <input
                 type="password"
-                className={`w-full px-4 py-2 border text-black ${
+                className={`w-full px-4 py-[7px] bg-zinc-800/50 text-white placeholder ${
                   errors.confirmPassword ? "border-red-500" : "border-gray-300"
-                } rounded focus:outline-none focus:ring-2 focus:ring-green-500`}
+                } rounded focus:outline-none focus:ring-1 focus:ring-green-500`}
                 value={confirmPassword}
                 onChange={(e) => setConfirmPassword(e.target.value)}
                 placeholder="Confirm your password"
