48032411816f90c3b63ce33a0e62899f83555710
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-09-15 12:30:18 +0530

    nuclear and calender page created

--- Uranium_Tracker_React/uranium-tracker/src/pages/Nuclear.jsx
+++ Uranium_Tracker_React/uranium-tracker/src/pages/Nuclear.jsx
@@ -1,11 +1,42 @@
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
+import { useDispatch, useSelector } from "react-redux";
+import { fetchNuclearData } from "../store/slices/apiSlice";
+import Loader from "../components/Loader"; // Import your loader component
+import { useEffect, useState } from "react";
 
 const Nuclear = () => {
-  return (
-    <div>
-      n
-    </div>
-  )
-}
+  const dispatch = useDispatch();
+  const nuclearData = useSelector((state) => state.api.nuclear);
+  console.log(nuclearData);
+  const status = useSelector((state) => state.api.status);
+  const [isLoading, setIsLoading] = useState(true); // Initialize isLoading state
 
-export default Nuclear
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchNuclearData());
+    }
+  }, [status, dispatch]); // Removed isLoading from dependency array
+
+  useEffect(() => {
+    if (status === "succeeded") {
+      setIsLoading(false); // Set loading to false when data is fetched
+    }
+  }, [status]); // Separate useEffect to handle loading state
+
+  return (
+    <>
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Navbar />
+          <Footer />
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Nuclear;
