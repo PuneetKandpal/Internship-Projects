f5055481a69e9eb511edec1c7e26efcd0c0b73b6
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-09-16 20:03:47 +0530

    Home comment

--- Uranium_Tracker_React/uranium-tracker/src/pages/Home.jsx
+++ Uranium_Tracker_React/uranium-tracker/src/pages/Home.jsx
@@ -5,9 +5,9 @@ import HomeHero from "../components/HomeHero";
 import HomeLosarsTableSec from "../components/HomeLosersTableSec";
 import HomePriceChart from "../components/HomePriceChart";
 import HomeTableNews from "../components/HomeTableNews";
-import HomeUraniumBlogs from "../components/HomeUraniumBlogs";
+// import HomeUraniumBlogs from "../components/HomeUraniumBlogs";
 import UraniumElementOfOpp from "../components/HomeUrElmOfOpportunity";
-import HomeVideoSec from "../components/HomeVideoSec";
+// import HomeVideoSec from "../components/HomeVideoSec";
 import Marquee from "../components/Marquee";
 import Navbar from "../components/Navbar";
 
