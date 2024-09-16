a3330114acf8d89f3996409946c91ce0a4dd272f
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-09-16 13:52:42 +0530

    AuthContext created

---
+++ Uranium_Tracker_React/uranium-tracker/src/context/AuthContext.jsx
@@ -0,0 +1,30 @@
+import { createContext, useState, useContext } from "react";
+
+// Create a Context for authentication
+const AuthContext = createContext();
+
+// Create a Provider component
+export const AuthProvider = ({ children }) => {
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    !!localStorage.getItem("token")
+  );
+
+  const login = (token) => {
+    localStorage.setItem("token", token);
+    setIsAuthenticated(true);
+  };
+
+  const logout = () => {
+    localStorage.removeItem("token");
+    setIsAuthenticated(false);
+  };
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+// Custom hook to use AuthContext
+export const useAuth = () => useContext(AuthContext);
