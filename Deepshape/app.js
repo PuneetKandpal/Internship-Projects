fd7130e9eaf4e1ae3408688f093f32a4624e48a1
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-09-18 09:28:37 +0530

    animation added to benefit section

--- Deepshape/app.js
+++ Deepshape/app.js
@@ -12,9 +12,7 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 
-
 // hero animation
-
 
 // features -----------------------------------------------------
 lucide.createIcons();
@@ -35,4 +33,37 @@ gsap.utils.toArray("[data-aos]").forEach((el) => {
       toggleActions: "play none none none",
     },
   });
+});
+
+// benefit sec animation
+gsap.registerPlugin(ScrollTrigger);
+
+// Text and Title Animation
+gsap.from(".py-20 [data-aos='fade']", {
+  opacity: 0,
+  y: 50,
+  duration: 1,
+  ease: "power3.out",
+  stagger: 0.3,
+  scrollTrigger: {
+    trigger: ".py-20",
+    start: "top 75%",
+    end: "bottom 25%",
+    toggleActions: "play none none none",
+  },
+});
+
+// Card Animation
+gsap.from(".grid .card[data-aos='scale']", {
+  opacity: 0,
+  scale: 0.9,
+  duration: 1,
+  ease: "power3.out",
+  stagger: 0.3,
+  scrollTrigger: {
+    trigger: ".py-20",
+    start: "top 75%",
+    end: "bottom 25%",
+    toggleActions: "play none none none",
+  },
 });
